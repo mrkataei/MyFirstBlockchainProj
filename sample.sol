d6d75e7614d26cfe50910c4364b3cd98b44c01f3
kourosh <kouroshinfo96@gmail.com>
2022-02-21 01:47:29 +0330

    view / pure non-state changing functions

--- sample.sol
+++ sample.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.6.0;
 contract SampleStorage {
     //this will get initialized to 0!
-    uint256 public favoriteNumber;
+    uint256 favoriteNumber;
     function store(uint256 _favoriteNumber) public{
         favoriteNumber = _favoriteNumber;
     }
     function retrieve() public view returns(uint256){
-        return favoriteNumber;
-}
+        return favoriteNumber; // read some state - getter function like
+    }
+    function process(uint256 _favoriteNumber) public pure{
+        favoriteNumber + _favoriteNumber; // not saving state anywhere
+    }
 }
