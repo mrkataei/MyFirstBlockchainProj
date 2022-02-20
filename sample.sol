1dc2c870986895e74053b2b288cc5d8911d2a21b
kourosh <kouroshinfo96@gmail.com>
2022-02-21 01:52:43 +0330

    struct

--- sample.sol
+++ sample.sol
@@ -1,7 +1,14 @@
 pragma solidity ^0.6.0;
 contract SampleStorage {
     //this will get initialized to 0!
+    // define var -> type visibility name
     uint256 favoriteNumber;
+    struct People {
+        // like object , different types
+        uint256 favoriteNumber;
+        string name;
+    }
+    People public person = People({favoriteNumber:2, name: "kourosh"}); // create object
     function store(uint256 _favoriteNumber) public{
         favoriteNumber = _favoriteNumber;
     }
