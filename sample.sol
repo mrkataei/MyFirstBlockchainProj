6b66d47e739563539abc817fc65fc8af80ec4738
kourosh <kouroshinfo96@gmail.com>
2022-02-21 01:43:26 +0330

    view / pure non-state changing functions

--- sample.sol
+++ sample.sol
@@ -5,4 +5,7 @@ contract SampleStorage {
     function store(uint256 _favoriteNumber) public{
         favoriteNumber = _favoriteNumber;
     }
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+}
 }
