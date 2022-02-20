e08f01a24f25c41fdddad4bcdaf24c1bceffa184
kourosh <kouroshinfo96@gmail.com>
2022-02-21 01:37:49 +0330

    updating variable

--- sample.sol
+++ sample.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.0;
 contract SampleStorage {
     //this will get initialized to 0!
-    uint256 favoriteNumber;
+    uint256 public favoriteNumber;
     function store(uint256 _favoriteNumber) public{
         favoriteNumber = _favoriteNumber;
     }
