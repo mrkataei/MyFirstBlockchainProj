12c90e457d61cfd29346a42e0f740165bf2b6ea4
kourosh <kouroshinfo96@gmail.com>
2022-04-24 12:11:13 +0430

    inheritanceContract.sol added

---
+++ inheritanceContract.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+// factory pattern
+// every storage factory contract will create simple contract
+import "./firstContract.sol";
+
+contract StorageFactory is SimpleStorage {
+    SimpleStorage [] public simpleStorageArray;
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+
+    }
+    function sfStore(uint _simpleStorageIndex, uint256 _simpleStorageNumber) public{
+        //Address
+        //ABI(application binary interface)
+        // store with address
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        // use view function of simple storage object in simpleStorageArray
+        // we find it with address key each object return it
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorage.retrieve();
+    }
+}
